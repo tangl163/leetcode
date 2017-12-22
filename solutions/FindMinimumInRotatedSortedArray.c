57a837baa2986f46230d27151f245bcc374e543e
tangl163 <cnjsw001@gmail.com>
2017-12-22 23:35:34 +0800

    Fixed bug

--- solutions/FindMinimumInRotatedSortedArray.c
+++ solutions/FindMinimumInRotatedSortedArray.c
@@ -9,7 +9,7 @@ findMin(int *nums, int len)
     while (low < high) {
         mid = (low + high) / 2;
 
-        if (nums[mid] >= nums[low])
+        if (nums[mid] >= nums[high])
             low = mid + 1;
         else
             high = mid;
