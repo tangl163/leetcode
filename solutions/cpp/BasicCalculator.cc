8a53366847c4a545d66490fe43d71899afe639ae
tangl163 <cnjsw001@gmail.com>
2021-03-30 22:24:35 +0800

    Improve performance

--- solutions/cpp/BasicCalculator.cc
+++ solutions/cpp/BasicCalculator.cc
@@ -1,133 +1,46 @@
+/**
+ * [reference](https://leetcode.com/problems/basic-calculator/discuss/62361/Iterative-Java-solution-with-stack)
+ */
 class Solution {
 public:
-    enum class Kind {
-        OPERATOR,
-        OPERAND,
-        PARENTHESES
-    };
-
     int calculate(string s) {
-        int result = 0;
         stack<int> stack;
-        auto postfix = infixToPostfix(s);
+        int sign, number, result;
 
-        for (auto &token : *postfix) {
-            auto kd = kind(token);
+        sign = 1;
+        number = result = 0;
 
-            if (kd == Kind::OPERAND) {
-                stack.push(std::stoi(token));
-            } else {
-                auto op2 = stack.top();
+        for (auto token : s) {
+            if (token >= '0' && token <= '9') {
+                number = number * 10 - '0' + token;
+            } else if (token == '+') {
+                result += number * sign;
+                number = 0;
+                sign = 1;
+            } else if (token == '-') {
+                result += number * sign;
+                number = 0;
+                sign = -1;
+            } else if (token == '(') {
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;
+                result = 0;
+                number = 0;
+            } else if (token == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.top();
                 stack.pop();
-                auto op1 = stack.top();
+                result += stack.top();
                 stack.pop();
-
-                if (token[0] == '+') {
-                    stack.push(op1 + op2);
-                } else {
-                    stack.push(op1 - op2);
-                }
             }
         }
 
-        return stack.top();
-    }
-private:
-    std::shared_ptr<vector<string>> infixToPostfix(const string s) {
-        string tmp;
-        stack<char> stack;
-        char token, preToken = 0;
-        auto input = std::istringstream(s);
-        auto ret = std::make_shared<vector<string>>();
-
-        while (input >> token) {
-            auto kd = kind(token);
-
-            if (kd == Kind::OPERATOR) {
-                if (preToken == 0 || preToken == '(') {
-                    ret->push_back("0");
-                }
-
-                if (!tmp.empty()) {
-                    ret->push_back(tmp);
-                    tmp.clear();
-                }
-
-                if (stack.empty()) {
-                    stack.push(token);
-                } else {
-                    auto top = stack.top();
-
-                    while (kind(top) == Kind::OPERATOR && !stack.empty()) {
-                        ret->emplace_back(1, top);
-                        stack.pop();
-
-                        if (stack.empty()) {
-                            break;
-                        }
-
-                        top = stack.top();
-                    }
-
-                    stack.push(token);
-                }
-            } else if (kd == Kind::OPERAND) {
-                tmp.append(1, token);
-            } else {
-                if (!tmp.empty()) {
-                    ret->push_back(tmp);
-                    tmp.clear();
-                }
-
-                if (token == '(') {
-                    stack.push(token);
-                } else {
-                    auto top = stack.top();
-                    while (top != '(') {
-                        ret->emplace_back(1, top);
-                        stack.pop();
-                        top = stack.top();
-                    }
-
-                    stack.pop();
-                }
-            }
-
-            preToken = token;
+        if (number != 0) {
+            result += number * sign;
         }
 
-        if (!tmp.empty()) {
-            ret->push_back(tmp);
-        }
-
-        while (!stack.empty()) {
-            ret->emplace_back(1, stack.top());
-            stack.pop();
-        }
-
-        return ret;
-    }
-
-    Kind kind(const char token) {
-        switch (token) {
-        case '+':
-        case '-':
-            return Kind::OPERATOR;
-        case '(':
-        case ')':
-            return Kind::PARENTHESES;
-        default:
-            return Kind::OPERAND;
-        }
-    }
-
-    Kind kind(const string &token) {
-        if (token.size() == 1) {
-            if (token[0] == '+' || token[0] == '-') {
-                return Kind::OPERATOR;
-            }
-        }
-
-        return Kind::OPERAND;
+        return result;
     }
 };
